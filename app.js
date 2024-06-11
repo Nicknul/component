d2fd24d5930ea7a0751cf03bb5ecc423a38c8ba5
정호연 <jea7730@gmail.com>
2024-06-11 10:55:35 +0900

    캡슐화 작업 예시

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-function a() {
+function a(name, age, job) {
   return {
-    name: '',
-    age: 0,
-    job: '',
+    name: name,
+    age: age,
+    job: job,
   };
 }
 
 // console.log(a());
 
-const b = a();
-
-console.log(b);
+const b = a('정호연', 27, '학생');
+console.log('첫번째 b:', b);
+b.address = '대전';
+console.log('두번째 b:', b);
